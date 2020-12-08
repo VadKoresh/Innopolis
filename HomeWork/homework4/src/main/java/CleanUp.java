24d8d1ce16bb45702ec693b4b93bd46c5975b722
Vadim Koreshkov <evilgod-2@mail.ru>
2020-12-08 23:01:19 +0300

    modified homework4 for lambda

--- HomeWork/homework4/src/main/java/CleanUp.java
+++ HomeWork/homework4/src/main/java/CleanUp.java
@@ -1,90 +1,91 @@
 import java.lang.reflect.Field;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class CleanUp {
 
-    void cleanup(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput){
+    void cleanup(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput) {
 
         Class<?> clazz = object.getClass();
 
         Class[] interfaces = clazz.getInterfaces();
-        for (Class interClazz: interfaces){
-            if (interClazz.getName().equals("Map")){
-                mapCleanOutput(object, fieldsToCleanup, fieldsToOutput);
+        for (Class interClazz : interfaces) {
+            if (interClazz.getName().equals("Map")) {
+                mapOutput(object, fieldsToOutput);
+                mapCleanup(object,fieldsToCleanup);
             }
         }
         fieldCleanUp(object, fieldsToCleanup, clazz);
         fieldOutput(object, fieldsToOutput, clazz);
     }
 
-    public void fieldCleanUp(Object object, Set<String> fieldsToCleanup, Class<?> clazz){
-        Iterator<String> iterator = fieldsToCleanup.iterator();
-
-        while (iterator.hasNext()){
+    public void fieldCleanUp(Object object, Set<String> fieldsToCleanup, Class<?> clazz) {
+        fieldsToCleanup.forEach((el -> {
             try {
-                Field field = clazz.getDeclaredField(iterator.next());
+                Field field = clazz.getDeclaredField(el);
                 field.setAccessible(true);
-                if (field.getType().isPrimitive()){
-                    if (field.getType().getName().equals("boolean")){
-                        field.setBoolean(object,false);
-                    }else if (field.getType().getName().equals("char")){
+                if (field.getType().isPrimitive()) {
+                    if (field.getType().getName().equals("boolean")) {
+                        field.setBoolean(object, false);
+                    } else if (field.getType().getName().equals("char")) {
                         field.setChar(object, '\u0000');
-                    }else {
+                    } else {
                         field.set(object, 0);
                     }
-                }else {
+                } else {
                     field.set(object, null);
                 }
             } catch (NoSuchFieldException | IllegalAccessException e) {
                 e.printStackTrace();
             }
         }
+
+        ));
+
     }
 
-    public void fieldOutput(Object object, Set<String> fieldsToOutput, Class<?> clazz){
-        Iterator<String> iterator = fieldsToOutput.iterator();
-
-        while (iterator.hasNext()){
+    public void fieldOutput(Object object, Set<String> fieldsToOutput, Class<?> clazz) {
+        fieldsToOutput.forEach((el -> {
             try {
-                Field field = clazz.getDeclaredField(iterator.next());
+                Field field = clazz.getDeclaredField(el);
                 field.setAccessible(true);
-                if (field.getType().isPrimitive()){
+                if (field.getType().isPrimitive()) {
                     System.out.println(String.valueOf(field.get(object)));
-                }else {
+                } else {
                     System.out.println(field.get(object).toString());
                 }
             } catch (NoSuchFieldException | IllegalAccessException e) {
                 e.printStackTrace();
             }
         }
+        ));
     }
 
-    public void mapCleanOutput(Object object, Set<String> fieldsToCleanup, Set<String> fieldsToOutput){
+    public void mapOutput(Object object, Set<String> fieldsToOutput) {
         Map<?, ?> map = (Map<?, ?>) object;
 
-        Iterator<String> iteratorClean = fieldsToCleanup.iterator();
-        Iterator<String> iteratorOut = fieldsToOutput.iterator();
-
-        while (iteratorClean.hasNext()) {
-            String key = iteratorClean.next();
-            if(map.containsKey(key)) {
-                map.remove(key);
+        fieldsToOutput.forEach((el -> {
+            if (map.containsKey(el)) {
+                System.out.println(map.get(el));
             } else {
                 throw new IllegalArgumentException("Поле отсутствует");
             }
-        }
-
-        while (iteratorOut.hasNext()) {
-            String key = iteratorOut.next();
-            if(map.containsKey(key)) {
-                System.out.println(map.get(key));
-            } else {
-                throw new IllegalArgumentException("Поле отсутствует");
-            }
-        }
+        }));
     }
+
+        public void mapCleanup(Object object, Set<String> fieldsToCleanup){
+            Map<?, ?> map = (Map<?, ?>) object;
+
+            fieldsToCleanup.forEach((el -> {
+                if (map.containsKey(el)) {
+                    map.remove(el);
+                } else {
+                    throw new IllegalArgumentException("Поле отсутствует");
+                }
+            }
+            ));
+        }
+
 
 
 }
